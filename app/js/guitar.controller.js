1b685e722075663f9a3e5003840039c72196d45d
Max Hewett <mjhewett01@gmail.com>
2015-06-25 16:15:23 -0400

    initial commit take 2

---
+++ app/js/guitar.controller.js
@@ -0,0 +1,15 @@
+;(function () {
+
+'use strict';
+
+angular.module('Guitars')
+
+
+.controller('Guitar', ['$scope', '$location']);
+
+
+
+
+}());
+
+
--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,7 @@ angular.module('Guitars', ['ngRoute'])
 .constant('PARSE', {
 
   URL: 'https://api.parse.com/1/',
-  config: {
+  CONFIG: {
     headers: {
 
       'X-Parse-Application-Id' : 'ShWkWAR0My9JW76mVc5A0OdFoFWEQEKRZrI8yUTI',
@@ -17,23 +17,46 @@ angular.module('Guitars', ['ngRoute'])
 
 })
 
-.config( ['$routeProvider'
+.config( ['$routeProvider',
 
 function ($routeProvider){
 
+    $routeProvider.when('/', {
 
-}
+      controller: 'GuitarPage',
+      templateUrl: 'js/home.tpl.html'
+
+    })
+
+
+  .when('/add', {
+
+    controller: 'GuitarPage',
+    templateUrl : 'js/add.tpl.html'
+
+  })
+
+  .when('/about', {
+
+    controller: 'GuitarPage',
+    templateUrl: 'js/about.tpl.html'
+    });
+
+  }
 
 ]);
 
-
-
-
-
-
-
-
-
-
-
 }());
+
+
+
+
+
+
+
+
+
+
+
+
+
